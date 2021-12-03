079fbb001ead67752f21204cb4f9df6172de4b41
Jacob Isber <jisber@vols.utk.edu>
2021-12-03 16:40:13 -0500

    Lab1

---
+++ lab1/lab1.cpp
@@ -0,0 +1,24 @@
+//Jacob Isber
+//Lab 1
+//Due 1.9.19
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+//Declared variable
+	string name; 
+
+//A
+	cout << "What is your name? ";
+
+	cin >> name;
+
+	cout << "Hello, " << name << "\n";
+
+
+
+return 1;
+
+}
